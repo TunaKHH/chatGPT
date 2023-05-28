74543288c81a79d51e72242ef2835211ca225644
Tuna <df95111111@gmail.com>
2023-05-28 21:17:56 +0800

    新增對話紀錄和系統訊息串流功能

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -4,7 +4,8 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from 'openai';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
@@ -13,7 +14,14 @@ import { Configuration, OpenAIApi } from 'openai';
 })
 export class ChatGateway {
   @WebSocketServer() server: Server;
+  private readonly logger = new Logger(ChatGateway.name);
   private openai: OpenAIApi;
+  private conversation: ChatCompletionRequestMessage[] = [];
+  // 系統訊息 ChatCompletionRequestMessage的格式
+  private systemDirections: ChatCompletionRequestMessage = {
+    role: 'system',
+    content: '通常情況下你總是用繁體中文回覆',
+  };
 
   constructor() {
     const configuration = new Configuration({
@@ -24,15 +32,24 @@ export class ChatGateway {
 
   @SubscribeMessage('message')
   async handleMessage(client: any, payload: any): Promise<void> {
-    const response = await this.openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: payload,
-      max_tokens: 150,
-      // n: 1,
-      temperature: 0.9,
+    this.conversation.push({
+      role: 'user',
+      content: payload,
+    }); // 將新的訊息加入對話紀錄
+    this.logger.debug('conversation', this.conversation);
+    this.conversation.push(this.systemDirections);
+    const response = await this.openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [...this.conversation, this.systemDirections],
+      max_tokens: 500,
+      temperature: 1,
     });
 
-    const message = response.data.choices[0].text.trim();
+    const message = response.data.choices[0].message.content.trim();
+    this.conversation.push({
+      role: 'assistant',
+      content: message,
+    }); // 將新的訊息加入對話紀錄
     this.server.emit('message', message);
   }
 }
